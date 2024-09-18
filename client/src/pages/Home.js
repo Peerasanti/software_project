bc629d0d1a9cdb1a7eeadda5ec837115fb39346e
jirapatkol <jirapat.ko@kkumail.com>
2024-09-19 00:59:32 +0700

    add code homecss

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import style from '../css/Home.css'; 
 
 function Home() {
 
