d05ba348faa34f5923bc9b8875b6618aed382a2d
GIT-BigTIM <110369291+GIT-BigTIM@users.noreply.github.com>
2024-09-22 19:11:09 +0700

    fix link

--- client/src/App.js
+++ client/src/App.js
@@ -41,42 +41,23 @@ function App() {
     <div className="App">
       <AuthContext.Provider value={{ authState, setAuthState }}>
         <Router>
-          <head></head>
 
-          <body class="vh-100 overflow-hidden">
+        {!authState.status ? (
+          <>
+            <Link to='/login'> Login </Link>
+            <Link to='/createuser'> Create User </Link>
+          </>
+          ) : (
+          <>
+            <button onClick={logout}> Logout </button>
+            <Link to={`/profile/${authState.id}`}> {authState.username} </Link>
+            <Link to='/cart'> Cart </Link> 
+            <Link to='/payment'> Payment </Link>
+          </>
+          )}
+          <Link to='/'> Home page </Link>
+          <Link to='/postArt'> Post Art </Link>
           
-            <nav class="navbar navbar-expand-lg bg-body-tertiary ">
-              <div class="container-fluid ">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                  <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNavDropdown">
-                  <ul class="navbar-nav">
-                    <li class="nav-item">
-                      <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Features</a>
-                    </li>
-                    <li class="nav-item">
-                      <a class="nav-link" href="#">Pricing</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                      <a class="nav-link dropdown-toggle" id="dropdownLink" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        Dropdown link
-                      </a>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                      </ul>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </nav>
-          </body>
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/createuser' element={<Register />} />
